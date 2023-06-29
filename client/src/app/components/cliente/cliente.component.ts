91c931410426dcaa6c185ebb2d962c2fc827d332
Juan Chanaga <juan.chanaga@artemisa.tech>
2023-06-29 15:22:16 -0500

    Ajustes clientes y tramos-clientes

--- client/src/app/components/cliente/cliente.component.ts
+++ client/src/app/components/cliente/cliente.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Inject, PLATFORM_ID} from '@angular/core';
+import {Fecha, Tramos} from "../../models/Graphics";
+import {GraphicsService} from "../../services/graphics.service";
+import * as am5 from "@amcharts/amcharts5";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5xy from "@amcharts/amcharts5/xy";
 
 @Component({
   selector: 'app-cliente',
@@ -6,5 +11,296 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente.component.css']
 })
 export class ClienteComponent {
+
+  @HostBinding('class') clases = 'row';
+
+  fechas: Fecha = {
+    fInicial: "",
+    fFinal: ""
+  }
+
+  constructor(private graphicsService: GraphicsService, @Inject(PLATFORM_ID) private platformId: Object) {
+  }
+
+  drawChart1(data: Tramos[]) {
+
+    let root = am5.Root.new("chartdiv1");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart = root.container.children.push(am5xy.XYChart.new(root, {
+      panX: false,
+      panY: false,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      layout: root.verticalLayout
+    }));
+
+    let legend = chart.children.push(
+      am5.Legend.new(root, {
+        centerX: am5.p50,
+        x: am5.p50
+      })
+    );
+
+    let xRenderer = am5xy.AxisRendererX.new(root, {
+      cellStartLocation: 0.1,
+      cellEndLocation: 0.9
+    })
+
+    let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+      categoryField: "Linea",
+      renderer: xRenderer,
+      tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    xRenderer.grid.template.setAll({
+      location: 1
+    })
+
+    xAxis.data.setAll(data);
+
+    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+      renderer: am5xy.AxisRendererY.new(root, {
+        strokeOpacity: 0.1
+      })
+    }));
+
+    function makeSeries(name: any, fieldName: any) {
+      let series = chart.series.push(am5xy.ColumnSeries.new(root, {
+        name: name,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: fieldName,
+        categoryXField: "Linea"
+      }));
+
+      series.columns.template.setAll({
+        tooltipText: "{name}, {categoryX}:{valueY}",
+        width: am5.percent(90),
+        tooltipY: 0,
+        strokeOpacity: 0
+      });
+
+      series.data.setAll(data);
+      series.appear();
+
+      series.bullets.push(function () {
+        return am5.Bullet.new(root, {
+          locationY: 0,
+          sprite: am5.Label.new(root, {
+            text: "{valueY}",
+            fill: root.interfaceColors.get("alternativeText"),
+            centerY: 0,
+            centerX: am5.p50,
+            populateText: true
+          })
+        });
+      });
+
+      legend.data.push(series);
+    }
+
+    makeSeries("Consumo Comercial", "consumo_comercial");
+    makeSeries("Costo Comercial", "costo_comercial");
+    makeSeries("Pérdidas Comercial", "perdidas_comercial");
+
+    chart.appear(1000, 100);
+  }
+
+  drawChart2(data: Tramos[]) {
+
+    let root = am5.Root.new("chartdiv2");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart = root.container.children.push(am5xy.XYChart.new(root, {
+      panX: false,
+      panY: false,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      layout: root.verticalLayout
+    }));
+
+    let legend = chart.children.push(
+      am5.Legend.new(root, {
+        centerX: am5.p50,
+        x: am5.p50
+      })
+    );
+
+    let xRenderer = am5xy.AxisRendererX.new(root, {
+      cellStartLocation: 0.1,
+      cellEndLocation: 0.9
+    })
+
+    let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+      categoryField: "Linea",
+      renderer: xRenderer,
+      tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    xRenderer.grid.template.setAll({
+      location: 1
+    })
+
+    xAxis.data.setAll(data);
+
+    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+      renderer: am5xy.AxisRendererY.new(root, {
+        strokeOpacity: 0.1
+      })
+    }));
+
+    function makeSeries(name: any, fieldName: any) {
+      let series = chart.series.push(am5xy.ColumnSeries.new(root, {
+        name: name,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: fieldName,
+        categoryXField: "Linea"
+      }));
+
+      series.columns.template.setAll({
+        tooltipText: "{name}, {categoryX}:{valueY}",
+        width: am5.percent(90),
+        tooltipY: 0,
+        strokeOpacity: 0
+      });
+
+      series.data.setAll(data);
+      series.appear();
+
+      series.bullets.push(function () {
+        return am5.Bullet.new(root, {
+          locationY: 0,
+          sprite: am5.Label.new(root, {
+            text: "{valueY}",
+            fill: root.interfaceColors.get("alternativeText"),
+            centerY: 0,
+            centerX: am5.p50,
+            populateText: true
+          })
+        });
+      });
+
+      legend.data.push(series);
+    }
+
+    makeSeries("Consumo Industrial", "consumo_industrial");
+    makeSeries("Costo Industrial", "costo_industrial");
+    makeSeries("Pérdidas Industrial", "perdidas_industrial");
+
+    chart.appear(1000, 100);
+  }
+
+  drawChart3(data: Tramos[]) {
+    let root = am5.Root.new("chartdiv3");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    let chart = root.container.children.push(am5xy.XYChart.new(root, {
+      panX: false,
+      panY: false,
+      wheelX: "panX",
+      wheelY: "zoomX",
+      layout: root.verticalLayout
+    }));
+
+    let legend = chart.children.push(
+      am5.Legend.new(root, {
+        centerX: am5.p50,
+        x: am5.p50
+      })
+    );
+
+    let xRenderer = am5xy.AxisRendererX.new(root, {
+      cellStartLocation: 0.1,
+      cellEndLocation: 0.9
+    })
+
+    let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+      categoryField: "Linea",
+      renderer: xRenderer,
+      tooltip: am5.Tooltip.new(root, {})
+    }));
+
+    xRenderer.grid.template.setAll({
+      location: 1
+    })
+
+    xAxis.data.setAll(data);
+
+    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+      renderer: am5xy.AxisRendererY.new(root, {
+        strokeOpacity: 0.1
+      })
+    }));
+
+    function makeSeries(name: any, fieldName: any) {
+      let series = chart.series.push(am5xy.ColumnSeries.new(root, {
+        name: name,
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: fieldName,
+        categoryXField: "Linea"
+      }));
+
+      series.columns.template.setAll({
+        tooltipText: "{name}, {categoryX}:{valueY}",
+        width: am5.percent(90),
+        tooltipY: 0,
+        strokeOpacity: 0
+      });
+
+      series.data.setAll(data);
+      series.appear();
+
+      series.bullets.push(function () {
+        return am5.Bullet.new(root, {
+          locationY: 0,
+          sprite: am5.Label.new(root, {
+            text: "{valueY}",
+            fill: root.interfaceColors.get("alternativeText"),
+            centerY: 0,
+            centerX: am5.p50,
+            populateText: true
+          })
+        });
+      });
+
+      legend.data.push(series);
+    }
+
+    makeSeries("Consumo Residencial", "consumo_residencial");
+    makeSeries("Costo Residencial", "costo_residencial");
+    makeSeries("Pérdidas Residencial", "perdidas_residencial");
+
+    chart.appear(1000, 100);
+  }
+
+  getCliente() {
+    this.graphicsService.getCliente(this.fechas)
+      .subscribe({
+          next: (response: Tramos[] | Object) => {
+            this.drawChart1(response as any);
+            this.drawChart2(response as any);
+            this.drawChart3(response as any);
+            window.document.getElementById('graficos')?.classList.remove('d-none');
+          },
+          error: (e) => console.error(e)
+        }
+      )
+  }
+
+  reset() {
+    window.location.reload();
+  }
 
 }
--- client/src/app/components/tramos-cliente/tramos-cliente.component.ts
+++ client/src/app/components/tramos-cliente/tramos-cliente.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Inject, PLATFORM_ID} from '@angular/core';
+import {Fecha, TramosCliente} from "../../models/Graphics";
+import {GraphicsService} from "../../services/graphics.service";
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-tramos-cliente',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./tramos-cliente.component.css']
 })
 export class TramosClienteComponent {
+
+  @HostBinding('class') clases = 'row';
+
+  fechas: Fecha = {
+    fInicial: "",
+    fFinal: ""
+  }
+
+  constructor(private graphicsService: GraphicsService, @Inject(PLATFORM_ID) private platformId: Object) {
+  }
+
+  addTableInfo(data: TramosCliente[]) {
+
+    data.forEach((fila, index) => {
+
+      let dataTable;
+
+      if (index % 2 === 0) {
+        dataTable = '<tr class="table-active">' +
+          '<td>' + fila.Linea + '</td>' +
+          '<td>' + fila.Perdidas + '</td>' +
+          '<td>' + fila.TipoConsumo + '</td>' +
+          '</tr>';
+      } else {
+        dataTable = '<tr class="table-light">' +
+          '<td>' + fila.Linea + '</td>' +
+          '<td>' + fila.Perdidas + '</td>' +
+          '<td>' + fila.TipoConsumo + '</td>' +
+          '</tr>';
+      }
+
+      $('#table-body').append(dataTable);
+    })
+  }
+
+  getTramosCliente() {
+    this.graphicsService.getTramosCliente(this.fechas)
+      .subscribe({
+          next: (response: TramosCliente[] | Object) => {
+            // this.drawChart1(response as any);
+            // this.drawChart2(response as any);
+            // this.drawChart3(response as any);
+            console.log(response);
+            this.addTableInfo(response as any);
+            window.document.getElementById('graficos')?.classList.remove('d-none');
+          },
+          error: (e) => console.error(e)
+        }
+      )
+  }
+
+  reset() {
+    window.location.reload();
+  }
 
 }
